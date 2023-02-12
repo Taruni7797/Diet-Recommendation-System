42193f881b51f4f3b815f6db41b840bf269c1b71
Sai Tharuni Samineni <94816105+Taruni7797@users.noreply.github.com>
2023-02-12 11:34:13 -0600

    Update diet.js

--- diet.js
+++ diet.js
@@ -1,1 +1,49 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JS Test</title>
+    <script src="main.js"></script>
+</head>
+
+<body>
+    <div class="content-wrapper">
+        <h1 id="heading">Test document for JS testing</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium officia similique illum magni explicabo,
+            tempore neque nulla laborum voluptas sint molestias libero et corporis omnis asperiores incidunt,
+            perferendis
+            sed aut!</p>
+
+            <button type="button" onclick="showAlert()">Show Alert</button>
+            <button type="button" onclick="updateHeading()">Update Heading</button>
+    </div>
+</body>
+
+</html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JS Test</title>
+    <script src="main.js"></script>
+</head>
+
+<body>
+    <div class="content-wrapper">
+        <h1 id="heading">Test document for JS testing</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium officia similique illum magni explicabo,
+            tempore neque nulla laborum voluptas sint molestias libero et corporis omnis asperiores incidunt,
+            perferendis
+            sed aut!</p>
+
+            <button type="button" onclick="showAlert()">Show Alert</button>
+            <button type="button" onclick="updateHeading()">Update Heading</button>
+    </div>
+</body>
+
+</html>
